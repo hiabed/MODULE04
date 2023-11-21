a5ff8a634e24f128088de382f64f41663ca5383f
Mohammed-Abed Hassani-Idrissi <mhassani@e1r8p5.1337.ma>
2023-11-21 15:55:31 +0100

    module04 ex00 in progress

---
+++ ex00/Animal.cpp
@@ -0,0 +1,16 @@
+#include "Animal.hpp"
+
+Animal::Animal()
+{
+    std::cout << "Animal constructor created!\n";
+}
+
+
+
+
+
+
+Animal::~Animal()
+{
+    std::cout << "Animal destructor created!\n";
+}
